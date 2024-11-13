e9661806eabc217a772eb9ba74a3b12410761626
Yashs1398 <yashprakash3104@gmail.com>
2024-11-14 01:56:01 +0530

    Modified by Yash

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import Footer from "./components/Footer";
+import { Toaster } from "react-hot-toast";
+
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element = {<HomePage/>} />
+      <Route path="/login" element = {<LoginPage/>} />
+      <Route path="/signup" element = {<SignUpPage/>} />
+    </Routes>
+    <Footer />
+    <Toaster />
+    </>
+  );
+}
+
+export default App
