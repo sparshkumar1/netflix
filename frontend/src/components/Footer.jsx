e4a166440981452a366301b655dc427d2745b580
Naveen-123-github <naveenskhadd2004@gmail.com>
2024-11-15 19:54:41 +0530

    Commit by Naveen

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -13,14 +13,14 @@ const Footer = () => {
 					</a>
 					. The source code is available on{" "}
 					<a
-						href='https://github.com/burakorkmez'
+						href='https://github.com/sparshkumar1/netflix'
 						target='_blank'
 						rel='noreferrer'
 						className='font-medium underline underline-offset-4'
 					>
 						GitHub
 					</a>
-					.
+					
 				</p>
 			</div>
 		</footer>
